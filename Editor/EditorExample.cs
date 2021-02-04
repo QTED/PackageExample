de3fe5d7e5de1fa6b2060cdb9604be6e96fcd917
QTEvD <agp62601@gmail.com>
2021-02-04 14:38:58 +0800

    add

---
+++ Editor/EditorExample.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class EditorExample
+{
+
+    static string version = "v0.1.0";
+
+    [MenuItem("QTED/EditorExample")]
+    static void EditorExampleFunc() {
+
+        Debug.Log("EditorExample: " + version);
+    }
+
+}
